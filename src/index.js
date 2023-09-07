fce1e9ffd14164dbaedd22a032f64700ee8b41bb
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-09-07 10:36:38 +0200

    Create default objects to assist with styling

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import Folder from "./folders.js"
 import {renderApplication} from "./render.js";
 
 const folders = [new Folder("Personal"), new Folder("Work")];
+const [personal] = folders;
+personal.addProject("Project", "High", "12 March", "Notes");
+personal.addFolderTask("Task", "Low", "2 January", "Notes");
 renderApplication(folders);
