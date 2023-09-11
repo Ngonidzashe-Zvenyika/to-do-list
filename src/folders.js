5d7095838e2269718bb359e37afd4c11d14bbef8
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-09-11 21:32:13 +0200

    Remove unnecessary functions in folders.js

--- src/folders.js
+++ src/folders.js
@@ -7,8 +7,14 @@ class Task {
         this.status = false;
         this.folder = folderName;
     }
-    toggleTaskStatus = () => {
+    toggleStatus = () => {
         this.status = (this.status === true) ? false : true;
+    }
+    editDetails = (name, priority, due, notes) => {
+        if (name) this.name = name;
+        if (priority) this.priority = priority;
+        if (due) this.due = due;
+        if (notes) this.notes = notes;
     }
 }
 
@@ -30,18 +36,6 @@ class Project extends Task {
         }
         this.tasks.splice(index, 1);
     }
-    editTask = (taskName, priority, due, notes, status) => {
-        const index = this.tasks.findIndex(task => task.name === taskName);
-        if (index === -1) {
-            console.log("Task not found");
-            return;
-        }
-        console.log(this.tasks[index]);
-        if (priority) this.tasks[index].priority = priority;
-        if (due) this.tasks[index].due = due;
-        if (notes) this.tasks[index].notes = notes;
-        if (status) (this.tasks[index].status) = (this.tasks[index].status) === false ? true : false;
-    }
 }
 
 export default class Folder {
@@ -50,29 +44,23 @@ export default class Folder {
         this.folderTasks = [];
         this.folderProjects = [];
     }
-    addFolderTask = (taskName, priority, due, notes) => {
+    addTask = (taskName, priority, due, notes) => {
         if (!(this.folderTasks.some(task => task.name === taskName))) {
             this.folderTasks.push(new Task(this.folderName, taskName, priority, due, notes));
         } else console.log("task exists in folder");
     }
-    addProject = (projectName, priority, due, notes) => {
-        if (!(this.folderProjects.some(project => project.name === projectName))) {
-            this.folderProjects.push(new Project(this.folderName, projectName, priority, due, notes));
-        } else console.log("project exists in folder");
-    }
-    addProjectTask = (projectName, taskName, priority, due, notes) => {
-        const project = this.folderProjects.find(project => project.name === projectName);
-        if (project === undefined) {
-            console.log("project doesnt exist in folder");
-        } else project.addTask(this.folderName, taskName, priority, due, notes);
-    }
-    removeFolderTask = (taskName) => {
+    removeTask = (taskName) => {
         const index = this.folderTasks.findIndex(task => task.name === taskName);
         if (index === -1) {
             console.log("Task not found");
             return;
         }
         this.folderTasks.splice(index, 1);
+    }
+    addProject = (projectName, priority, due, notes) => {
+        if (!(this.folderProjects.some(project => project.name === projectName))) {
+            this.folderProjects.push(new Project(this.folderName, projectName, priority, due, notes));
+        } else console.log("project exists in folder");
     }
     removeProject = (projectName) => {
         const index = this.folderProjects.findIndex(project => project.name === projectName);
@@ -81,46 +69,6 @@ export default class Folder {
             return;
         }
         this.folderProjects.splice(index, 1);
-    }
-    removeProjectTask = (projectName, taskName) => {
-        const index = this.folderProjects.findIndex(project => project.name === projectName);
-        if (index === -1) {
-            console.log("Project not found");
-            return;
-        }
-        this.folderProjects[index].removeTask(taskName);
-    }
-    editFolderTask = (taskName, priority, due, notes, status) => {
-        const index = this.folderTasks.findIndex(task => task.name === taskName);
-        if (index === -1) {
-            console.log("Task not found");
-            return;
-        }
-        console.log(this.folderTasks[index]);
-        if (priority) this.folderTasks[index].priority = priority;
-        if (due) this.folderTasks[index].due = due;
-        if (notes) this.folderTasks[index].notes = notes;
-        if (status) (this.folderTasks[index].status) = (this.folderTasks[index].status) === false ? true : false;
-    }
-    editProject = (projectName, priority, due, notes, status) => {
-        const index = this.folderProjects.findIndex(project => project.name === projectName);
-        if (index === -1) {
-            console.log("Project not found");
-            return;
-        }
-        console.log(this.folderProjects[index]);
-        if (priority) this.folderProjects[index].priority = priority;
-        if (due) this.folderProjects[index].due = due;
-        if (notes) this.folderProjects[index].notes = notes;
-        if (status) (this.folderProjects[index].status) = (this.folderProjects[index].status) === false ? true : false;
-    }
-    editProjectTask = (projectName, taskName, priority, due, notes, status) => {
-        const index = this.folderProjects.findIndex(project => project.name === projectName);
-        if (index === -1) {
-            console.log("Project not found");
-            return;
-        }
-        this.folderProjects[index].editTask(taskName, priority, due, notes, status);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const folders = [new Folder("Personal"), new Folder("Work")];
 const [personal] = folders;
 personal.addProject("Project", "High", "12 March", "Notes");
 personal.addProject("Another Project", "High", "12 March", "Notes");
-personal.addProjectTask("Project")
-personal.addProjectTask("Project", "")
-personal.addProjectTask("Project", "j")
-personal.addFolderTask("Task", "Low", "2 January", "Notes");
-personal.addFolderTask("Another Task", "Low", "2 January", "Notes");
+personal.folderProjects[0].addTask("")
+personal.folderProjects[0].addTask("Task", "")
+personal.folderProjects[0].addTask("Psdsfdfs", "Low")
+personal.addTask("Task", "Low", "2 January", "Notes");
+personal.addTask("Another Task", "Low", "2 January", "Notes");
 renderApplication(folders);
